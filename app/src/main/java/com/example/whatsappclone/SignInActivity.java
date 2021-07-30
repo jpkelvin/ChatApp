ec96df904630a57275e79ac56908abe9a0048b20
umesh chandra <umeshchandra1999>
2021-07-30 14:46:59 +0530

    completed till 5

--- app/src/main/java/com/example/whatsappclone/SignInActivity.java
+++ app/src/main/java/com/example/whatsappclone/SignInActivity.java
@@ -1,12 +1,18 @@
 package com.example.whatsappclone;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.whatsappclone.databinding.ActivitySignInBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -20,7 +26,7 @@ public class SignInActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding=ActivitySignInBinding.inflate(getLayoutInflater());
+        binding = ActivitySignInBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         getSupportActionBar().hide();
@@ -30,13 +36,47 @@ public class SignInActivity extends AppCompatActivity {
 
         progressDialog = new ProgressDialog(SignInActivity.this);
         progressDialog.setTitle("Login");
-        progressDialog.setMessage("Please Wait\n,Validation in Progress");
+        progressDialog.setMessage("Please Wait\n,Validation in Progress.");
 
         binding.btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                progressDialog.show();
+                if(!binding.txtEmail.getText().toString().isEmpty() && !binding.txtPassword.getText().toString().isEmpty())
+                {
+                    progressDialog.show();
+                    mAuth.signInWithEmailAndPassword(binding.txtEmail.getText().toString(),binding.txtPassword.getText().toString())
+                         .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                             @Override
+                             public void onComplete(@NonNull Task<AuthResult> task) {
+                                 progressDialog.dismiss();
+                                 if(task.isSuccessful()){
+                                     Intent intent = new Intent(SignInActivity.this, MainActivity.class);
+                                     startActivity(intent);
+                                 }
+                                 else
+                                 {
+                                     Toast.makeText(SignInActivity.this,task.getException().getMessage() , Toast.LENGTH_SHORT).show();
+                                 }
+                             }
+                         });
+
+                }
+
+                else{
+                    Toast.makeText(SignInActivity.this, "enter credentials", Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
+
+        binding.txtClicksignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignInActivity.this,SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
