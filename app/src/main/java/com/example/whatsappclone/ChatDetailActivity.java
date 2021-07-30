12c4d12848153a7b81f66951d14c2b4c27333cc8
umesh chandra <umeshchandra1999>
2021-07-31 00:03:18 +0530

    completed till 10th video - No Error

---
+++ app/src/main/java/com/example/whatsappclone/ChatDetailActivity.java
@@ -0,0 +1,46 @@
+package com.example.whatsappclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.whatsappclone.databinding.ActivityChatDetailBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
+
+public class ChatDetailActivity extends AppCompatActivity {
+
+    ActivityChatDetailBinding binding;
+
+    FirebaseAuth mAuth;
+    FirebaseDatabase firebaseDatabase;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityChatDetailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        getSupportActionBar().hide();
+
+        mAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+
+        final String senderId = mAuth.getUid();
+        String userId = getIntent().getStringExtra("userId");
+        String userName = getIntent().getStringExtra("userName");
+        String profilePic = getIntent().getStringExtra("profilePic");
+        binding.userName.setText(userName);
+        Picasso.get().load(profilePic).placeholder(R.drawable.avatar).into(binding.profileImage);
+
+        binding.backArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ChatDetailActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
